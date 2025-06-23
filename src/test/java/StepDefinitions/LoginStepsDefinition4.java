938b7f27aaa241b4a50c547e2745c81bc773ec58
tanmayadintelle <tanmay.nayak@adintelle.com>
2025-06-23 17:01:10 +0530

    code changes

--- src/test/java/StepDefinitions/LoginStepsDefinition4.java
+++ src/test/java/StepDefinitions/LoginStepsDefinition4.java
@@ -549,9 +549,9 @@ public class LoginStepsDefinition4 {
 	         
 			String Rate = row.getCell(7).toString();//*[@id="action-dialog-delete"]/div/table/tr/td[8]/input
 			System.out.println("Rate is"+Rate);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='action-dialog-delete']/div/table/tr/td[8]/input")));
-			wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='action-dialog-delete']/div/table/tr/td[8]/input")));
-		    WebElement RateField = driver.findElement(By.xpath("//*[@id='action-dialog-delete']/div/table/tr/td[8]/input"));
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Rate")));
+			wait1.until(ExpectedConditions.elementToBeClickable(By.name("Rate")));
+		    WebElement RateField = driver.findElement(By.name("Rate"));
 		    RateField.clear();
 			RateField.sendKeys(Rate);
 //		    RateField.sendKeys(Keys.ENTER);
