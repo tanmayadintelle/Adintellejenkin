d36ce2e277e60b525b9bdf80dbdd9acadb0fb270
tanmayadintelle <tanmay.nayak@adintelle.com>
2025-06-23 16:31:25 +0530

    code

--- src/test/java/StepDefinitions/LoginStepsDefinition4.java
+++ src/test/java/StepDefinitions/LoginStepsDefinition4.java
@@ -547,7 +547,8 @@ public class LoginStepsDefinition4 {
 			Thread.sleep(2000);
 			Thread.sleep(6000);
 	         
-			String Rate = row.getCell(7).toString();
+			String Rate = row.getCell(7).toString();//*[@id="action-dialog-delete"]/div/table/tr/td[8]/input
+			System.out.println("Rate is"+Rate);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"action-dialog-delete\"]/div/table/tr/td[8]/input")));
 			wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"action-dialog-delete\"]/div/table/tr/td[8]/input")));
 		    WebElement RateField = driver.findElement(By.xpath("//*[@id=\"action-dialog-delete\"]/div/table/tr/td[8]/input"));
