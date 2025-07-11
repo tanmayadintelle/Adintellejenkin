519d3084ff97afcd5bd04a4406f5acf28daa2d02
tanmayadintelle <tanmay.nayak@adintelle.com>
2025-07-11 14:34:18 +0530

    code changes

--- src/test/java/StepDefinitions/LoginStepsDefinition4.java
+++ src/test/java/StepDefinitions/LoginStepsDefinition4.java
@@ -946,10 +946,13 @@ public class LoginStepsDefinition4 {
 		    ((JavascriptExecutor) driver).executeScript("document.body.style.zoom='50%'");
 		    wait1.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#planning > div > table > thead > tr > th.mat-mdc-header-cell.mdc-data-table__header-cell.cdk-header-cell.cdk-column-select.mat-column-select.ng-star-inserted > input")));
 			driver.findElement(By.cssSelector("#planning > div > table > thead > tr > th.mat-mdc-header-cell.mdc-data-table__header-cell.cdk-header-cell.cdk-column-select.mat-column-select.ng-star-inserted > input")).click();
+			 ((JavascriptExecutor) driver).executeScript("document.body.style.zoom='50%'");
 			wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/div/div/div/main/div/app-estimate/div/div[3]/div[2]/span[2]")));
 			//driver.findElement(By.cssSelector("#planning > div > table > thead > tr > th.mat-mdc-header-cell.mdc-data-table__header-cell.cdk-header-cell.cdk-column-select.mat-column-select.ng-star-inserted > input")).click();
 			driver.findElement(By.xpath("/html/body/app-root/div/div/div/main/div/app-estimate/div/div[3]/div[2]/span[2]")).click();
+			 ((JavascriptExecutor) driver).executeScript("document.body.style.zoom='50%'");
 			wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/div/div/div/main/div/app-estimate/div/div[5]/div/div[2]/div[1]/div[2]/div/div/div/mat-datepicker-toggle/button/span[3]")));
+			
 			WebElement calendarButton1 = driver.findElement(By.xpath("/html/body/app-root/div/div/div/main/div/app-estimate/div/div[5]/div/div[2]/div[1]/div[2]/div/div/div/mat-datepicker-toggle/button/span[3]"));
 			calendarButton1.click();
 			
