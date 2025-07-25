6a5cc7e20b9ccc9904e02c229fd8c6cb9c6eccc8
tanmayadintelle <tanmay.nayak@adintelle.com>
2025-07-25 11:36:27 +0530

    campaignnamec

--- src/test/java/StepDefinitions/LoginStepsDefinition7.java
+++ src/test/java/StepDefinitions/LoginStepsDefinition7.java
@@ -253,7 +253,7 @@ public void user_createsnewjob_and_addsacampaign() throws InterruptedException, 
 	 WebDriverWait waitload = new WebDriverWait(driver, Duration.ofSeconds(1200));
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
     JavascriptExecutor js = (JavascriptExecutor) driver;
-    ((JavascriptExecutor) driver).executeScript("document.body.style.zoom='40%'");
+    ((JavascriptExecutor) driver).executeScript("document.body.style.zoom='80%'");
  // Wait for the Client dropdown input to be ready
     //((JavascriptExecutor) driver).executeScript("document.body.style.zoom='40%'");
     waitload.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"select-client\"]/div/div[1]/ng-select/div")));
