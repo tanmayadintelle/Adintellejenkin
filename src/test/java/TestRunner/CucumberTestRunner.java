5998a0bf7abde2e049b534b68dc6f31718ccbc88
tanmayadintelle <tanmay.nayak@adintelle.com>
2025-07-28 12:10:02 +0530

    campaignnamec

--- src/test/java/TestRunner/CucumberTestRunner.java
+++ src/test/java/TestRunner/CucumberTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
-@CucumberOptions(tags="",features= {"src/test/resources/Features/NewDigitalPROSanityVBF.feature"},glue= {"StepDefinitions"},plugin= {"pretty","html:target/htmlreport.html"},  monochrome = true)
+@CucumberOptions(tags="",features= {"src/test/resources/Features/NewDigitalPROSanityVBF.feature","src/test/resources/Features/NewDigitalPROSanityCBF.feature"},glue= {"StepDefinitions"},plugin= {"pretty","html:target/htmlreport.html"},  monochrome = true)
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests{	 
 	
